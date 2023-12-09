35c635c0b2d2cd8be031c1bf56bcdd4b1a48303c
Marinakrae1 <marinakraehh@gmail.com>
2023-12-09 19:53:34 -0300

    feat: mainbody e footer

--- frontend/wallet-pila/src/App.jsx
+++ frontend/wallet-pila/src/App.jsx
@@ -1,11 +1,15 @@
+import { Footer } from "./components/footer";
+import { Mainbody } from "./components/mainbody";
 import { Topbanner } from "./components/topbanner";
 import { Topbar } from "./components/topbar";
 import "./global.css"
 function App() {
   return (
    <>
-   <Topbar></Topbar>
-   <Topbanner></Topbanner>
+   <Topbar/>
+   <Topbanner/>
+   <Mainbody/>
+   <Footer/>
    </>
   );
 }
---
+++ frontend/wallet-pila/src/components/mainbody/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export function Mainbody(){
+    return(
+        <div className="mainbody">
+            alguma coisa
+        </div>
+    )
+}
