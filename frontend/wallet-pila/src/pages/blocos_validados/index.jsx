bf3c0636e4a1578c7768c464dab83f55b6ef82ae
Marinakrae1 <marinakraehh@gmail.com>
2023-12-10 22:44:32 -0300

    Update index.jsx

--- frontend/wallet-pila/src/pages/blocos_validados/index.jsx
+++ frontend/wallet-pila/src/pages/blocos_validados/index.jsx
@@ -5,7 +5,7 @@ export function BlocosValidados(){
     return (
         <div className="content">
             <h1>Blocos Validados</h1>
-            Validando bloco mienrado pelo(a): iris_augusto<br/>
+            Validando bloco minerado pelo(a): iris_augusto<br/>
             9537269629856648250451285778281024414972490165930051618004409463251537
             110427941548649020598956093796432407239217743554726184882600387580788735<br/>
             Numero do bloco: 17336<br/>
