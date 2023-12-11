02ddfa647c71f0ee311ce93e75b16163b4d037fc
Pedro Ortiz <pedrops.ortiz@gmail.com>
2023-12-10 22:27:12 -0300

    why are sundasy so depressing

--- frontend/wallet-pila/src/pages/blocos_validados/index.jsx
+++ frontend/wallet-pila/src/pages/blocos_validados/index.jsx
@@ -5,7 +5,11 @@ export function BlocosValidados(){
     return (
         <div className="content">
             <h1>Blocos Validados</h1>
-            <div>BLOCOS</div>
+            Validando bloco mienrado pelo(a): iris_augusto<br/>
+            9537269629856648250451285778281024414972490165930051618004409463251537
+            110427941548649020598956093796432407239217743554726184882600387580788735<br/>
+            Numero do bloco: 17336<br/>
+            Valido!<br/>
         </div>
     );
 }
--- frontend/wallet-pila/src/pages/pilas_validados/index.jsx
+++ frontend/wallet-pila/src/pages/pilas_validados/index.jsx
@@ -4,7 +4,9 @@ export function PilasValidados(){
     return (
         <div className="content">
             <h1>Pilas Validados</h1>
-            <div>PILAS</div>
+            Validando pila do(a): ewerton-joaokunde<br/>
+            Valido!<br/>
+            Validando pila do(a): ewerton-joaokunde<br/>
         </div>
     );
 }
